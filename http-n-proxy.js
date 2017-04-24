7edfae63309b3cc9f8af3fca38ef12858b62b187
Martin Etmajer <martin@etmajer.com>
2017-04-24 08:12:05 +0200

    Fix log message and indentation. Add basic error handler.

--- http-n-proxy.js
+++ http-n-proxy.js
@@ -4,17 +4,21 @@ var http = require('http'),
     httpProxy = require('http-proxy');
 
 if (!process.env.HTTP_N_PROXY_TARGETS) {
-  console.log('Failed to determine proxy targets: environment variable N_PROXY_TARGETS has not been set');
+  console.log('Failed to determine proxy targets: environment variable HTTP_N_PROXY_TARGETS has not been set');
   process.exit(1);
 }
 
-var proxy = httpProxy.createProxyServer({});
+var proxy = httpProxy.createProxyServer({})
+  .on('error', function (err) {
+    console.log(e);
+  });
+
 var proxyPort = process.env.HTTP_N_PROXY_PORT || 80;
 var proxyTargets = process.env.HTTP_N_PROXY_TARGETS.split(/, ?/);
 
 var server = http.createServer(function(req, res) {
   proxyTargets.forEach(function(value) {
-  	proxy.web(req, res, { target: value });
+    proxy.web(req, res, { target: value });
   });
 });
 
