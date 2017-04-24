d097229acec01082aa40d749ecac78e4d56b813e
Martin Etmajer <martin@etmajer.com>
2017-04-24 08:11:36 +0200

    Do local builds and enable verbose logging.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-s2i build https://github.com/metmajer/http-n-proxy.git centos/nodejs-44-centos7 metmajer/http-n-proxy
+s2i build . centos/nodejs-44-centos7 metmajer/http-n-proxy --copy --loglevel=5
